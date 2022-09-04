49d9c559252f67ef4d46f4a0116b05c663f875ec
Splatage.com <81358782+splatage@users.noreply.github.com>
2022-09-04 13:50:58 +1200

    Update deploy.pl

--- deploy.pl
+++ deploy.pl
@@ -162,7 +162,7 @@ foreach my $game (keys %{$settings}) {
 #app->renderer->cache->max_keys(0);
 app->sessions->default_expiration( 24 * 60 * 60 );
 
-my $salt = pack "C16", map { int( 128 * rand() ) } 0 .. 15;
+my $salt = pack "C16", map { int( 64 * rand() + 25 ) } 0 .. 15;
 
 app->yancy->plugin(
     'Auth::Password' => {
