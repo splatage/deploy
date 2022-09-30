9e9deec277957683916e28f4514a7e13132f6988
BuildTools <unconfigured@null.spigotmc.org>
2022-10-01 10:24:18 +1300

    update startup script

--- start.sh
+++ start.sh
@@ -1,34 +1,58 @@
 #!/bin/bash
+MINION_WORKERS=2
+MINION_JOBS=4
+MOJO_APP=deploy.pl
 
-function start () {
-#    screen -h 1024 -L -dmS mojo   perl deploy.pl prefork -m production -l http://*:3000
-    MOJO_REVERSE_PROXY=1 hypnotoad deploy.pl 
-    screen -h 1024 -L -dmS minion1 perl deploy.pl minion worker -m production -I 15 -C 5 -R 3600 -j 4
-    screen -h 1024 -L -dmS minion2 perl deploy.pl minion worker -m production -I 15 -C 5 -R 3600 -j 4
-    screen -h 1024 -L -dmS minion3 perl deploy.pl minion worker -m production -I 15 -C 5 -R 3600 -j 4
+function start_mojo () {
+    hypnotoad deploy.pl
+}
+
+function start_minions () {
+    until [  ${MINION_WORKERS} -eq 0 ]; do
+        echo "starting minion worker number ${MINION_WORKERS}"
+	echo "screen -h 1024 -L -dmS minion${MINION_WORKERS} perl deploy.pl minion worker -m production -I 15 -C 5 -R 3600 -j ${MINION_JOBS}"
+        screen -h 1024 -L -dmS minion${MINION_WORKERS} perl ${MOJO_APP} minion worker -m production -I 15 -C 5 -R 3600 -j ${MINION_JOBS}
+        let MINION_WORKERS-=1
+    done 
+
     sleep 1
     screen -list
 }
 
 function test () {
-    screen -h 1024 -L -dmS mojo_test1 morbo deploy.pl -l http://*:3001
+    screen -h 1024 -L -dmS mojo_test1 morbo ${MOJO_APP} -l http://*:3001
     sleep 1
     screen -list
 }
 
-function stop () {
+function stop_minions () {
     screen -list | awk -F '.' '/mojo/ || /minion/ {print $1}' | xargs kill
-    hypnotoad -s deploy.pl
     sleep 2
     screen -list
 }
 
+function stop_mojo () {
+    hypnotoad -s ${MOJO_APP}
+}
+
+function hot_restart_mojo () {
+    hypnotoad ${MOJO_APP}
+}
+
+
 case $1 in
+    restart)
+        hot_restart_mojo
+        stop_minions
+        start_minions
+    ;;
     start)
-        start
+        start_mojo
+        start_minions
     ;;
     stop)
-        stop
+        stop_mojo
+        stop_minions
     ;;
     test)
         test
